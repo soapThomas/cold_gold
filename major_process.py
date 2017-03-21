d1382096b8027537668cbd24cf82b8c05c159907
Thomas <thomascodings@gmail.com>
2017-03-22 00:49:26 +0800

    modify aa

--- major_process.py
+++ major_process.py
@@ -13,30 +13,30 @@ if __name__ == "__main__":
 
     if "Windows" in platform.system():
         bp_txt = "\\annotate_pro\\input_go_files\\bp.txt"
-        nr_fas = "\\nr_pro\\input_nr\\uniprot_sprot.fasta"
+        nr_fas = "\\nr_pro\\input_nr\\nr_30.fasta"
         sample_text = "\\annotate_pro\\input_go_files\\sample.txt"
         sample_fas = "\\nr_pro\\input_nr\\sample.fasta"
 
     else:
         bp_txt = "/annotate_pro/input_go_files/bp.txt"
-        nr_fas = "/nr_pro/input_nr/uniprot_sprot.fasta"
+        nr_fas = "/nr_pro/input_nr/nr_30.fasta"
         sample_text = "/annotate_pro/input_go_files/sample.txt"
         sample_fas = "/nr_pro/input_nr/sample.fasta"
 
     # first_part:
-    tmp_dic = parse_file_by_ori(current_dir + sample_text)
+    tmp_dic = parse_file_by_ori(current_dir + bp_txt)
     print tmp_dic
-    pro_list = parse_fasta_use_bio(current_dir + sample_fas)
+    pro_list = parse_fasta_use_bio(current_dir + nr_fas)
     print pro_list
     #
-    # count = 0
-    # for i in pro_list:
-    #     if i in tmp_dic.keys():
-    #         count += 1
-    #         # print i
-    # print count
-    # print len(tmp_dic)
-    # print len(pro_list)
+    count = 0
+    for i in pro_list:
+        if i in tmp_dic.keys():
+            count += 1
+            # print i
+    print count
+    print len(tmp_dic)
+    print len(pro_list)
 
     # second part
     # 10.141.211.65作为跳板机
