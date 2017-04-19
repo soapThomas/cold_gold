5edca761379282bc1ea067d4535bf51e4599b201
Thomas <thomascodings@gmail.com>
2017-04-19 18:13:46 +0800

    add nwalign cp

--- lib/nwalign/exec_align.py
+++ lib/nwalign/exec_align.py
@@ -1,6 +1,40 @@
 # coding utf-8
 
+import os, sys
 from lib.remote.remoter import *
+
+
+# FILE_PATH = os.getcwd() +
+REMOTE_NWALIGN_PATH = "/home/thomas/packageNWalign"
+ROOT_USER = "root"
+ROOT_PASSWD = "qwdong123"
+
+LOCAL_NWALIGN_FILE = "/home/thomas/NWalign"
+REMOTE_NWALIGN_FILE = "/home/thomas/packageNWalign/NWalign"
+
+LOCAL_FASTA_PATH = "/home/thomas/test"
+REMOTE_FASTA_PATH = "/home/thomas/packageNWalign/fasta"
+
+IP_LIST = ["10.141.211.66", "10.141.211.67", "10.141.211.68", "10.141.211.69"]
+
+
+def deploy_nwalign(ip):
+
+    # create nwalign dir
+    mkdir_cmd = "mkdir -p {0}".format(REMOTE_NWALIGN_PATH)
+    remote_execute(ip, ROOT_USER, ROOT_PASSWD, mkdir_cmd)
+
+    # copy_file_to remote nwalign dir
+    copy_file_to(ip, ROOT_USER, ROOT_PASSWD, LOCAL_NWALIGN_FILE, REMOTE_NWALIGN_FILE)
+
+    # make nwalign executable
+    chmod_cmd = "chmod +x {0}".format(REMOTE_NWALIGN_FILE)
+    remote_execute(ip, ROOT_USER, ROOT_PASSWD, chmod_cmd)
+
+
+def deploy_fasta(ip):
+    scp_cmd = "scp -r {0} {1}@{2}:{3}".format(LOCAL_FASTA_PATH, ROOT_USER, ip, REMOTE_FASTA_PATH)
+    remote_execute(ip, ROOT_USER, ROOT_PASSWD, scp_cmd)
 
 
 def exec_align(fasta_A, fasta_B, result_file):
@@ -17,7 +51,13 @@ def exec_align(fasta_A, fasta_B, result_file):
 
 
 def split_task():
-    pass
+    """
+
+    :return:
+    """
+    file_path = "F:\\test"
+    for parent, dirnames, filenames in os.walk(file_path):
+        print filenames
 
 
 def fetch_result(file_name):
@@ -32,4 +72,9 @@ def fetch_result(file_name):
     return identity[1]
 
 if __name__ == "__main__":
-    print fetch_result("sample_1_2.txt")
+    # print fetch_result("sample_1_2.txt")
+    # print PathController.get_root_path()
+    # split_task()
+    for i in IP_LIST:
+        deploy_nwalign(i)
+        deploy_fasta(i)
--- lib/remote/remoter.py
+++ lib/remote/remoter.py
@@ -11,3 +11,13 @@ def remote_execute(remote_ip, user, passwd, cmd):
     ssh.exec_command(cmd)
     ssh.close()
 
+
+def copy_file_to(remote_ip, user, passwd, local_file_path, remote_file_path):
+    ssh = paramiko.Transport(remote_ip, 22)
+    ssh.connect(username=user, password=passwd)
+    sftp = paramiko.SFTPClient.from_transport(ssh)
+    print "{0} Local file:".format("127.0.0.1"), local_file_path
+    print "{0} Remote file:".format(remote_ip), remote_file_path
+    sftp.put(local_file_path, remote_file_path)
+    sftp.close()
+    ssh.close()
