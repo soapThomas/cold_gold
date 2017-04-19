7d705577e9dc2b8752785710f76cad407a76e4de
Thomas <thomascodings@gmail.com>
2017-04-20 01:01:06 +0800

    add data shaixuan

--- major_process.py
+++ major_process.py
@@ -13,30 +13,33 @@ if __name__ == "__main__":
 
     if "Windows" in platform.system():
         bp_txt = "\\annotate_pro\\input_go_files\\bp.txt"
-        nr_fas = "\\nr_pro\\input_nr\\nr_30.fasta"
+        nr_fas = "\\nr_pro\\input_nr\\data_30.fasta"
         sample_text = "\\annotate_pro\\input_go_files\\sample.txt"
         sample_fas = "\\nr_pro\\input_nr\\sample.fasta"
 
     else:
         bp_txt = "/annotate_pro/input_go_files/bp.txt"
-        nr_fas = "/nr_pro/input_nr/nr_30.fasta"
+        nr_fas = "/nr_pro/input_nr/data_30.fasta"
         sample_text = "/annotate_pro/input_go_files/sample.txt"
         sample_fas = "/nr_pro/input_nr/sample.fasta"
 
     # first_part:
     tmp_dic = parse_file_by_ori(current_dir + bp_txt)
-    print tmp_dic
+    # print tmp_dic
+
     pro_list = parse_fasta_use_bio(current_dir + nr_fas)
-    print pro_list
+    # print pro_list
     #
+
     count = 0
     for i in pro_list:
         if i in tmp_dic.keys():
+            print i
             count += 1
             # print i
-    print count
-    print len(tmp_dic)
-    print len(pro_list)
+    print count          # 2457
+    print len(tmp_dic)   # 15960
+    print len(pro_list)  # 55321
 
     # second part
     # 10.141.211.65作为跳板机
